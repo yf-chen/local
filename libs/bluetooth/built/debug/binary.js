72a4acf2efdc8d54ffef8268135b2549387012ad
821060457@qq.com <chenyongfei>
2021-05-07 10:00:55 +0800

    11

---
+++ libs/bluetooth/built/debug/binary.js
@@ -0,0 +1,235 @@
+// total=6697 new=68.34% cached=0.00% other=31.66%
+(function (ectx) {
+'use strict';
+const runtime = ectx.runtime;
+const oops = ectx.oops;
+const doNothing = ectx.doNothing;
+const pxsim = ectx.pxsim;
+const globals = ectx.globals;
+const maybeYield = ectx.maybeYield;
+const setupDebugger = ectx.setupDebugger;
+const isBreakFrame = ectx.isBreakFrame;
+const breakpoint = ectx.breakpoint;
+const trace = ectx.trace;
+const checkStack = ectx.checkStack;
+const leave = ectx.leave;
+const checkResumeConsumed = ectx.checkResumeConsumed;
+const setupResume = ectx.setupResume;
+const setupLambda = ectx.setupLambda;
+const checkSubtype = ectx.checkSubtype;
+const failedCast = ectx.failedCast;
+const buildResume = ectx.buildResume;
+const mkVTable = ectx.mkVTable;
+const bind = ectx.bind;
+const leaveAccessor = ectx.leaveAccessor;
+const __this = runtime;
+const pxtrt = pxsim.pxtrt;
+let yieldSteps = 1;
+ectx.setupYield(function() { yieldSteps = 100; })
+pxsim.setTitle("bluetooth");
+pxsim.setConfigData({}, {});
+pxtrt.mapKeyNames = [
+ ""
+];
+__this.setupPerfCounters([]);
+const pxsim_Array__getAt = pxsim.Array_.getAt;
+const pxsim_Array__length = pxsim.Array_.length;
+const pxsim_Array__mk = pxsim.Array_.mk;
+const pxsim_Array__push = pxsim.Array_.push;
+const pxsim_Boolean__bang = pxsim.Boolean_.bang;
+const pxsim_String__concat = pxsim.String_.concat;
+const pxsim_String__stringConv = pxsim.String_.stringConv;
+const pxsim_numops_toBool = pxsim.numops.toBool;
+const pxsim_numops_toBoolDecr = pxsim.numops.toBoolDecr;
+const pxsim_pxtcore_mkAction = pxsim.pxtcore.mkAction;
+const pxsim_pxtcore_mkClassInstance = pxsim.pxtcore.mkClassInstance;
+const pxsim_pxtrt_ldlocRef = pxsim.pxtrt.ldlocRef;
+const pxsim_pxtrt_mapGetByString = pxsim.pxtrt.mapGetByString;
+const pxsim_pxtrt_stclo = pxsim.pxtrt.stclo;
+const pxsim_pxtrt_stlocRef = pxsim.pxtrt.stlocRef;
+const pxsim_Boolean_ = pxsim.Boolean_;
+const pxsim_pxtcore = pxsim.pxtcore;
+const pxsim_String_ = pxsim.String_;
+const pxsim_ImageMethods = pxsim.ImageMethods;
+const pxsim_Array_ = pxsim.Array_;
+const pxsim_pxtrt = pxsim.pxtrt;
+const pxsim_numops = pxsim.numops;
+
+
+function _main___P39336(s) {
+let r0 = s.r0, step = s.pc;
+s.pc = -1;
+
+
+while (true) {
+if (yieldSteps-- < 0 && maybeYield(s, step, r0) || runtime !== pxsim.runtime) return null;
+switch (step) {
+  case 0:
+
+    globals._intervals___39722 = (undefined);
+    globals._pollEventQueue___39938 = (undefined);
+    s.tmp_0 = console_addListener__P39743_mk(s);
+    s.tmp_0.arg0 = inline__P40031;
+    s.callLocIdx = 1; s.pc = 1; return s.tmp_0;
+  case 1:
+    r0 = s.retval;
+    r0 = undefined;
+    return leave(s, r0)
+  default: oops()
+} } }
+_main___P39336.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"bluetooth.ts","functionName":"<main>","argumentNames":[]}
+_main___P39336.continuations = [  ]
+
+function _main___P39336_mk(s) {
+    checkStack(s.depth);
+    return {
+        parent: s, fn: _main___P39336, depth: s.depth + 1,
+        pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
+  tmp_0: undefined,
+} }
+
+
+
+
+
+function inline__P40031(s) {
+let r0 = s.r0, step = s.pc;
+s.pc = -1;
+
+
+while (true) {
+if (yieldSteps-- < 0 && maybeYield(s, step, r0) || runtime !== pxsim.runtime) return null;
+switch (step) {
+  case 0:
+
+    if (s.lambdaArgs) {
+      s.arg0 = (s.lambdaArgs[0]);
+      s.arg1 = (s.lambdaArgs[1]);
+      s.lambdaArgs = null;
+    }
+    r0 = undefined;
+    r0 = undefined;
+    return leave(s, r0)
+  default: oops()
+} } }
+inline__P40031.info = {"start":363,"length":41,"line":15,"column":24,"endLine":15,"endColumn":65,"fileName":"bluetooth.ts","functionName":"inline","argumentNames":["_pri","msg"]}
+
+function inline__P40031_mk(s) {
+    checkStack(s.depth);
+    return {
+        parent: s, fn: inline__P40031, depth: s.depth + 1,
+        pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
+  arg0: undefined,
+  arg1: undefined,
+} }
+
+
+
+
+
+function bluetooth___log__P40029(s) {
+let r0 = s.r0, step = s.pc;
+s.pc = -1;
+
+
+while (true) {
+if (yieldSteps-- < 0 && maybeYield(s, step, r0) || runtime !== pxsim.runtime) return null;
+switch (step) {
+  case 0:
+
+    if (s.lambdaArgs) {
+      s.arg0 = (s.lambdaArgs[0]);
+      s.arg1 = (s.lambdaArgs[1]);
+      s.lambdaArgs = null;
+    }
+    r0 = undefined;
+    return leave(s, r0)
+  default: oops()
+} } }
+bluetooth___log__P40029.info = {"start":262,"length":76,"line":12,"column":4,"endLine":14,"endColumn":5,"fileName":"bluetooth.ts","functionName":"__log","argumentNames":["priority","msg"]}
+
+function bluetooth___log__P40029_mk(s) {
+    checkStack(s.depth);
+    return {
+        parent: s, fn: bluetooth___log__P40029, depth: s.depth + 1,
+        pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
+  arg0: undefined,
+  arg1: undefined,
+} }
+
+
+
+
+
+function console_addListener__P39743(s) {
+let r0 = s.r0, step = s.pc;
+s.pc = -1;
+
+
+while (true) {
+if (yieldSteps-- < 0 && maybeYield(s, step, r0) || runtime !== pxsim.runtime) return null;
+switch (step) {
+  case 0:
+
+    if (s.lambdaArgs) {
+      s.arg0 = (s.lambdaArgs[0]);
+      s.lambdaArgs = null;
+    }
+    r0 = pxsim_numops_toBoolDecr(globals.listeners___39735);
+    s.tmp_1 = r0;
+    r0 = pxsim_Boolean__bang(s.tmp_1);
+    s.tmp_0 = r0;
+    r0 = pxsim_numops_toBoolDecr(s.tmp_0);
+    if (!r0) { step = 1; continue; }
+    r0 = pxsim_Array__mk();
+    globals.listeners___39735 = (r0);
+  case 1:
+  case 2:
+    r0 = pxsim_numops_toBoolDecr(s.arg0);
+    s.tmp_1 = r0;
+    r0 = pxsim_Boolean__bang(s.tmp_1);
+    s.tmp_0 = r0;
+    r0 = pxsim_numops_toBool(s.tmp_0);
+    if (!r0) { step = 3; continue; }
+    r0 = s.tmp_0;
+    { step = 4; continue; }
+  case 3:
+    r0 = pxsim_Array_.indexOf(globals.listeners___39735, s.arg0, undefined);
+    s.tmp_2 = r0;
+    r0 = (s.tmp_2 > -1);
+  case 4:
+    // jmp value (already in r0)
+    s.tmp_3 = r0;
+    r0 = pxsim_numops_toBoolDecr(s.tmp_3);
+    if (!r0) { step = 5; continue; }
+    { step = 7; continue; }
+  case 5:
+  case 6:
+    r0 = pxsim_Array__push(globals.listeners___39735, s.arg0);
+  case 7:
+    r0 = undefined;
+    return leave(s, r0)
+  default: oops()
+} } }
+console_addListener__P39743.info = {"start":3264,"length":236,"line":114,"column":4,"endLine":118,"endColumn":5,"fileName":"pxt_modules/core/console.ts","functionName":"addListener","argumentNames":["listener"]}
+
+function console_addListener__P39743_mk(s) {
+    checkStack(s.depth);
+    return {
+        parent: s, fn: console_addListener__P39743, depth: s.depth + 1,
+        pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
+  tmp_0: undefined,
+  tmp_1: undefined,
+  tmp_2: undefined,
+  tmp_3: undefined,
+  arg0: undefined,
+} }
+
+
+
+
+
+const breakpoints = setupDebugger(1, [])
+
+return _main___P39336
+})
---
+++ libs/radio-broadcast/built/debug/binary.js
@@ -0,0 +1,92 @@
+// total=2916 new=27.30% cached=0.00% other=72.70%
+(function (ectx) {
+'use strict';
+const runtime = ectx.runtime;
+const oops = ectx.oops;
+const doNothing = ectx.doNothing;
+const pxsim = ectx.pxsim;
+const globals = ectx.globals;
+const maybeYield = ectx.maybeYield;
+const setupDebugger = ectx.setupDebugger;
+const isBreakFrame = ectx.isBreakFrame;
+const breakpoint = ectx.breakpoint;
+const trace = ectx.trace;
+const checkStack = ectx.checkStack;
+const leave = ectx.leave;
+const checkResumeConsumed = ectx.checkResumeConsumed;
+const setupResume = ectx.setupResume;
+const setupLambda = ectx.setupLambda;
+const checkSubtype = ectx.checkSubtype;
+const failedCast = ectx.failedCast;
+const buildResume = ectx.buildResume;
+const mkVTable = ectx.mkVTable;
+const bind = ectx.bind;
+const leaveAccessor = ectx.leaveAccessor;
+const __this = runtime;
+const pxtrt = pxsim.pxtrt;
+let yieldSteps = 1;
+ectx.setupYield(function() { yieldSteps = 100; })
+pxsim.setTitle("radio-broadcast");
+pxsim.setConfigData({}, {});
+pxtrt.mapKeyNames = [
+ ""
+];
+__this.setupPerfCounters([]);
+const pxsim_Array__getAt = pxsim.Array_.getAt;
+const pxsim_Array__length = pxsim.Array_.length;
+const pxsim_Array__mk = pxsim.Array_.mk;
+const pxsim_Array__push = pxsim.Array_.push;
+const pxsim_Boolean__bang = pxsim.Boolean_.bang;
+const pxsim_String__concat = pxsim.String_.concat;
+const pxsim_String__stringConv = pxsim.String_.stringConv;
+const pxsim_numops_toBool = pxsim.numops.toBool;
+const pxsim_numops_toBoolDecr = pxsim.numops.toBoolDecr;
+const pxsim_pxtcore_mkAction = pxsim.pxtcore.mkAction;
+const pxsim_pxtcore_mkClassInstance = pxsim.pxtcore.mkClassInstance;
+const pxsim_pxtrt_ldlocRef = pxsim.pxtrt.ldlocRef;
+const pxsim_pxtrt_mapGetByString = pxsim.pxtrt.mapGetByString;
+const pxsim_pxtrt_stclo = pxsim.pxtrt.stclo;
+const pxsim_pxtrt_stlocRef = pxsim.pxtrt.stlocRef;
+const pxsim_Boolean_ = pxsim.Boolean_;
+const pxsim_pxtcore = pxsim.pxtcore;
+const pxsim_String_ = pxsim.String_;
+const pxsim_ImageMethods = pxsim.ImageMethods;
+const pxsim_Array_ = pxsim.Array_;
+const pxsim_pxtrt = pxsim.pxtrt;
+const pxsim_numops = pxsim.numops;
+
+
+function _main___P45242(s) {
+let r0 = s.r0, step = s.pc;
+s.pc = -1;
+
+
+while (true) {
+if (yieldSteps-- < 0 && maybeYield(s, step, r0) || runtime !== pxsim.runtime) return null;
+switch (step) {
+  case 0:
+
+    globals._intervals___45628 = (undefined);
+    globals._pollEventQueue___45844 = (undefined);
+    r0 = undefined;
+    return leave(s, r0)
+  default: oops()
+} } }
+_main___P45242.info = {"start":0,"length":0,"line":0,"column":0,"endLine":0,"endColumn":0,"fileName":"radio-broadcast.ts","functionName":"<main>","argumentNames":[]}
+_main___P45242.continuations = [  ]
+
+function _main___P45242_mk(s) {
+    checkStack(s.depth);
+    return {
+        parent: s, fn: _main___P45242, depth: s.depth + 1,
+        pc: 0, retval: undefined, r0: undefined, overwrittenPC: false, lambdaArgs: null,
+} }
+
+
+
+
+
+const breakpoints = setupDebugger(1, [])
+
+return _main___P45242
+})
